25fe43030679e1f67f557d2297babda862ed5b86
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2022-08-27 16:14:13 +0100

    :wrench: Adicionado a função para pegar os valor na base de dados.

--- index.js
+++ index.js
@@ -6,98 +6,95 @@ const Jogo = require("./db/Jogo");
 
 //DATABASE
 connection
-    .authenticate()
-    .then(() => {
-        console.log("Conexão com banco de dados feita com sucesso!")
-    })
-    .catch((msgErro) => {
-        console.log("Algo deu errado.")
-    })
+  .authenticate()
+  .then(() => {
+    console.log("Conexão com banco de dados feita com sucesso!");
+  })
+  .catch((msgErro) => {
+    console.log("Algo deu errado.");
+  });
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/jogos", (req, res) => {
-    console.log(Jogo)
-    res.status(200).json({Jogo})
+app.get("/jogos", async (req, res) => {
+  const jogos = await Jogo.findAll();
+  res.json(jogos);
 });
 
 app.get("/games/:id", (req, res) => {
-    if (isNaN(req.params.id)) {
-        res.sendStatus(400)
-    } else {
-        var id = parseInt(req.params.id);
-        var game = Jogo.game.find(g => g.id == id);
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var game = Jogo.game.find((g) => g.id == id);
 
-        if(game != undefined) {
-            res.statusCode = 200;
-            res.json(game)
-        } else {
-            res.sendStatus(404);
-        };
-    };
+    if (game != undefined) {
+      res.statusCode = 200;
+      res.json(game);
+    } else {
+      res.sendStatus(404);
+    }
+  }
 });
 
 app.post("/game", (req, res) => {
+  var { title, price, year } = req.body;
 
-    var {title, price, year} = req.body;
+  Jogo.create({
+    title: title,
+    price: price,
+    year: year,
+  });
 
-    Jogo.create({
-        title:title,
-        price:price,
-        year:year
-    });
-
-    res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 app.delete("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var index = Jogo.games.findIndex((g) => g.id == id);
 
-    if (isNaN(req.params.id)) {
-        res.sendStatus(400)
+    if (index == -1) {
+      res.sendStatus(404);
     } else {
-        var id = parseInt(req.params.id);
-        var index = Jogo.games.findIndex(g => g.id == id);
-
-        if(index == -1) {
-            res.sendStatus(404);
-        } else {
-            Jogo.games.splice(index,1);
-            res.sendStatus(200);
-        }
+      Jogo.games.splice(index, 1);
+      res.sendStatus(200);
     }
-})
+  }
+});
 
 app.put("/game/:id", (req, res) => {
-    if (isNaN(req.params.id)) {
-        res.sendStatus(400)
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+    var game = Jogo.games.find((g) => g.id == id);
+
+    if (game != undefined) {
+      var { title, price, year } = req.body;
+
+      if (title != undefined) {
+        game.title = title;
+      }
+
+      if (price != undefined) {
+        game.price = price;
+      }
+
+      if (year != undefined) {
+        game.year = year;
+      }
+
+      res.sendStatus(200);
     } else {
-        var id = parseInt(req.params.id);
-        var game = Jogo.games.find(g => g.id == id);
-
-        if(game != undefined) {
-            var {title, price, year} = req.body;
-
-            if(title != undefined) {
-                game.title = title
-            }
-
-            if(price != undefined) {
-                game.price = price;
-            }
-
-            if(year != undefined) {
-                game.year = year;
-            }
-
-            res.sendStatus(200);
-
-        } else {
-            res.sendStatus(404);
-        }
+      res.sendStatus(404);
     }
-})
+  }
+});
 
 app.listen(45678, () => {
-    console.log("API RODANDO!")
+  console.log("API RODANDO!");
 });
